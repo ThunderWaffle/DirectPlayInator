4e853b811b985b97e74283eb583eb7b5fd0876b6
ThunderWaffle <thunder@xboxchaos.com>
2018-01-13 00:01:04 -0600

    vast improvement to command building and file parsing code

--- script.py
+++ script.py
@@ -1,7 +1,9 @@
 import subprocess
+import json
 import re
 import os
 import os.path
+from pprint import pprint
 
 def bash_command(cmd):
     with open('output.txt', 'w+') as f:
@@ -10,139 +12,132 @@ def bash_command(cmd):
 def get_filename_no_ext(filename):
 	filename_parts = re.split("\.", filename)[:-1]
 	return '.'.join(filename_parts)
-	
-def verify_success(filename, new_filename):
-	if os.path.isfile(new_filename):
-		if os.path.getsize(new_filename) > 0:
-			with open("successful.txt", "a") as myfile:
-				myfile.write(filename + "," + new_filename)
-			return
-	
-	with open("failed.txt", "a") as myfile:
-		myfile.write(filename + "," + new_filename)
-	os.remove(new_filename)
 
 def parse_codecs(filename):
-	bash_command(["./ffmpeg.exe", "-i", filename ]).wait()
+	bash_command(["./ffprobe.exe", "-v", "quiet", "-of", "json", "-show_streams", "-show_format", filename]).wait()
 
 	with open('output.txt') as f:
-		content = f.readlines()
-
-	video_streams = []
-	audio_streams = []
-	subtitle_streams = []
-
-	for line in content:
-		if line.startswith("  Duration"):
-			line = line[2:]
-			split_line = re.split(" ", line)
-
-			#bitrate is element 5
-			bitrate = int(split_line[5])
-
-		if line.startswith("    Stream #0"):
-			line = line[4:]
-			split_line = re.split(" ", line)
-			
-			#codec type is always element 3
-			if split_line[2] == 'Video:':
-				codec = split_line[3].rstrip("\r\n")
-				video_streams.append(codec)
-
-			elif split_line[2] == 'Audio:':
-				codec = split_line[3].rstrip("\r\n")
-				audio_streams.append(codec)
-
-			elif split_line[2] == 'Subtitle:':
-				codec = split_line[3].rstrip("\r\n")
-				subtitle_streams.append(codec)
-        
-	return bitrate, video_streams, audio_streams, subtitle_streams
+		json_data = json.load(f)
+	
+	container_streams = []
+	
+	streams = json_data['streams']
+	
+	
+	for stream in streams:
+		stream_struct = {}
+		stream_struct['type'] = stream['codec_type']
+		stream_struct['codec'] = stream['codec_name']
+		stream_struct['language'] = 'und'
+		disposition = stream['disposition']
+		stream_struct['default'] = disposition['default']
+		stream_struct['forced'] = disposition['forced']
+		if stream_struct['type'] == 'audio':
+			stream_struct['channels'] = stream['channels']
+		if 'tags' in stream:
+			tags = stream['tags']
+			if 'language' in tags:
+				stream_struct['language'] = tags['language']
+		container_streams.append(stream_struct)
+		
+	container_structure = {}
+	container_structure['streams'] = container_streams
+	
+	container_structure['bitrate'] = json_data['format']['bit_rate']
+	container_structure['length'] = json_data['format']['duration']
+	
+	return container_structure
 	
 
-def convert_file(filename, bitrate, video_streams, audio_streams, subtitle_streams):
-	
-	#making assumption that the only video stream we would care about is the first one.
-	source_is_h264 = video_streams[0] == 'h264'
-	source_bitrate_ok = bitrate < 8000
-	source_all_aac = True
-	source_no_subtitles = len(subtitle_streams) == 0
+def convert_file(filename, container_structure):
 	
 	filename_no_ext = get_filename_no_ext(filename)
-	new_filename = filename_no_ext + "-new"
+	new_filename = filename_no_ext + "-new.mp4"
 	
-	for stream in audio_streams:
-		if stream != 'aac':
-			source_all_aac = False
+	i = 0
+	
+	video_stream_index = -1
+	audio_stream_index = -1
+	
+	preamble_args = []
+	input_args = []
+	map_args = []
+	video_args = []
+	audio_args = []
+	
+	input_args.extend(["-i", filename])
+	
+	#build up video and audio right now only!
+	for i in range(len(container_structure['streams'])):
+		stream = container_structure['streams'][i]
+		if stream['default'] == 0 and stream['forced'] == 0:
+			continue
 			
-	#the best case, all codecs are good to go; just write a new file
-	if source_is_h264 and source_bitrate_ok and source_all_aac :
-		new_filename_ext = new_filename + '.mp4'
-		print "No codec conversion necessary!"
-		print "Writing new file... " + new_filename_ext
-		bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "copy", "-c:a", "copy", new_filename_ext ]).wait()
-		
-	#just need to convert audio
-	elif source_is_h264 and source_bitrate_ok and source_no_subtitles:
-		new_filename_ext = new_filename + '.mp4'
-		print "Only audio codec conversion required!"
-		print "Writing new file... " + new_filename_ext
-		bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "copy", "-c:a", "libfdk_aac", "-vbr", "4", new_filename_ext ]).wait()
-		
-	# full transcode required
-	elif source_no_subtitles:
-		new_filename_ext = new_filename + '.mp4'
-		print "Full transcode in progress..."
-		print "Writing new file... " + new_filename_ext
-		bash_command(["./ffmpeg.exe", "-y", "-hwaccel", "cuvid", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "h264_nvenc", "-preset", "slow", "-profile:v", "high", "-b:v", "7M", "-c:a", "libfdk_aac", "-vbr", "4", new_filename_ext ]).wait()		
-		
-	#codecs fine but subtitles exist so switching to MKV
-	elif source_is_h264 and source_bitrate_ok and source_all_aac:
-		new_filename_ext = new_filename + '.mkv'
-		print "No codec conversion necessary; subtitles found"
-		print "Writing new file... " + new_filename_ext
-		cmd = bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0", "-c:v", "copy", "-c:a", "copy", "-c:s", "srt", new_filename_ext ])
-		cmd.wait()
-		# conversion of subtitles FAILED, just remove them and use MP4
-		if cmd.returncode != 0:
-			os.remove(new_filename_ext)
-			print "Subtitle conversion failed (probably image based).  Removing and retrying as MP4"
-			new_filename_ext = new_filename + '.mp4'
-			bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "copy", "-c:a", "copy", new_filename_ext ]).wait()
+		if stream['type'] == 'video' and video_stream_index == -1:
+			video_stream_index = i
 			
-	#subtitles exist and audio codec is wrong
-	elif source_is_h264 and source_bitrate_ok:
-		new_filename_ext = new_filename + '.mkv'
-		print "Only audio codec conversion required; subtitles found"
-		print "Writing new file... " + new_filename_ext
-		cmd = bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0", "-c:v", "copy", "-c:a", "libfdk_aac", "-vbr", "4", "-c:s", "srt", new_filename_ext ])
-		cmd.wait()
-		# conversion of subtitles FAILED, just remove them and use MP4
-		if cmd.returncode != 0:
-			os.remove(new_filename_ext)
-			print "Subtitle conversion failed (probably image based).  Removing and retrying as MP4"				
-			new_filename_ext = new_filename + '.mp4'
-			bash_command(["./ffmpeg.exe", "-y", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "copy", "-c:a", "libfdk_aac", "-vbr", "4", new_filename_ext ]).wait()
-		
-	#worst case :(.  full transcode with subtitles thrown in
-	else:
-		new_filename_ext = new_filename + '.mkv'
-		print "Full transcode required; subtitles found"
-		print "Writing new file... " + new_filename_ext
-		cmd = bash_command(["./ffmpeg.exe", "-y", "-hwaccel", "cuvid", "-i", filename, "-map", "0", "-c:v", "h264_nvenc", "-preset", "slow", "-profile:v", "high", "-b:v", "7M", "-c:a", "libfdk_aac", "-vbr", "4", "-c:s", "srt", new_filename_ext])
-		cmd.wait()
-		# conversion of subtitles FAILED, just remove them and use MP4
-		if cmd.returncode != 0:
-			os.remove(new_filename_ext)
-			print "Subtitle conversion failed (probably image based).  Removing and retrying as MP4"
-			new_filename_ext = new_filename + '.mp4'
-			bash_command(["./ffmpeg.exe", "-y", "-hwaccel", "cuvid", "-i", filename, "-map", "0:v", "-map", "0:a", "-c:v", "h264_nvenc", "-preset", "slow", "-profile:v", "high", "-b:v", "7M", "-c:a", "libfdk_aac", "-vbr", "4", new_filename_ext]).wait()
+			map_str = "0:" + str(i)
+			map_args.extend(["-map", map_str])	
 			
-	verify_success(filename, new_filename_ext)		
+			if int(container_structure['bitrate']) > 7500000 or stream['codec'] != 'h264':
+				preamble_args.extend(["-hwaccel", "cuvid"])
+				video_args.extend(["-c:v:0", "h264_nvenc", "-preset", "slow", "-profile:v", "high", "-b:v", "7M"])
+			else:
+				video_args.extend(["-c:v:0", "copy"])
+				
 		
+		if stream['type'] == 'audio' and audio_stream_index == -1:
+			audio_stream_index = i
+			
+			map_str = "0:" + str(i)
+			
+			#leave audio alone if its stereo AAC
+			if stream['channels'] <= 2 and stream['codec'] == 'aac':
+				map_args.extend(["-map", map_str])			
+				audio_args.extend(["-c:a:0", "copy"])
+				
+			elif stream['channels'] <= 5:
+				map_args.extend(["-map", map_str])
+				audio_args.extend(["-c:a:0", "aac", "-ac", "2"])
+				
+			else:
+				#ugh we have to make separate files and recombine them later.
+				surround_command = ["./ffmpeg.exe", "-y", "-i", filename, "-map", map_str, "-c:a:0", "libfdk_aac", "-ac", "6", "6channel.mp4"]
+				bash_command(surround_command).wait()
+				stereo_command = ["./ffmpeg.exe", "-y", "-i", filename, "-map", map_str, "-c:a:0", "libfdk_aac", "-ac", "2", "2channel.mp4"]
+				bash_command(stereo_command).wait()
+				
+				input_args.extend(["-i", "2channel.mp4"])
+				input_args.extend(["-i", "6channel.mp4"])
+				
+				map_args.extend(["-map", "1:0"])
+				map_args.extend(["-map", "2:0"])
+				
+				audio_args.extend(["-c:a:0", "copy"])
+				audio_args.extend(["-c:a:1", "copy"])
+				
+				
+	print "Writing new file... " + new_filename
+	
+	full_command = ["./ffmpeg.exe", "-y"]
+	full_command.extend(preamble_args)
+	full_command.extend(input_args)	
+	full_command.extend(map_args)
+	full_command.extend(video_args)
+	full_command.extend(audio_args)
+	full_command.append(new_filename)
+	print full_command
+	bash_command(full_command).wait()
+	os.remove("2channel.mp4")
+	os.remove("6channel.mp4")
+	
+	#TODO external subtitles...
+
+	
 with open('files_to_convert.txt', 'r') as f:
 	content = f.readlines()
 	for line in content:
 		file_to_convert = line.rstrip("\r\n")
-		bitrate, video_streams, audio_streams, subtitle_streams = parse_codecs(file_to_convert)
-		convert_file(file_to_convert, bitrate, video_streams, audio_streams, subtitle_streams)
+		parse_codecs(file_to_convert)
+		container_struct = parse_codecs(file_to_convert)
+		convert_file(file_to_convert, container_struct)
